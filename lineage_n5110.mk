63bdac9418ddd6927d65d77743f74b350a640527
html6405 <peter.schelchshorn@mhs-solutions.at>
2021-02-14 14:42:28 +0100

    lineage_n5110.mk: Change boot animation resolution.

--- lineage_n5110.mk
+++ lineage_n5110.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 600
+
 # Inherit some common lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
